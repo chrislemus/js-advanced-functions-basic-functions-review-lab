d150b418012b030e5b74dc7a29faa168e9787974
chrislemus <icrislemus@gmail.com>
2021-03-17 07:30:19 -0400

    commit tree 77a416795d8bf77adeb18f847666e0d359c86db2

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+
+function wrapAdjective(highlight='*') {
+  return function(param='special') {
+    const wrappedAdj = highlight + param + highlight;
+    return `You are ${wrappedAdj}!`
+  }
+}
+
+const Calculator = {
+  add: (num1, num2) => {return num1 + num2},
+  subtract: (num1, num2) => {return num1 - num2},
+  multiply: (num1, num2) => {return num1 * num2},
+  divide: (num1, num2) => {return num1 / num2},
+}
+
+function actionApplyer(int, funcs) {
+  if (funcs.length > 0) {
+    return funcs.reduce((acc, func) =>  func(acc) , int)
+  } else {
+    return int
+  }
+}
+
+// wrapAdjective("%")("a dedicated programmer") //=&gt; "You are %a dedicated programmer%!"
